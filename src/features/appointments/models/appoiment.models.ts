3b2dcdb0d169bcdbe6ba174f415d877a5f79ffe2
EmilzonJ <emilzonujero@gmail.com>
2022-11-28 12:41:22 -0600

    Add[appointments][AppoinmentList]: GetAll

--- src/features/appointments/models/appoiment.models.ts
+++ src/features/appointments/models/appoiment.models.ts
@@ -1,5 +1,11 @@
 
 export interface Appointment {
   id: string;
-  customer_name: string;
+  customer: string;
+  customerPhone: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  status: string;
+  userId: string;
 }
